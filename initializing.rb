577fe5d84f2692c9ae81cb39c9a0b3cf251df650
hildakh <h_khajehamiri@yahoo.com>
2019-12-08 21:59:53 +0000

    initializing a new object

---
+++ initializing.rb
@@ -0,0 +1,8 @@
+class GoodDog
+  def initialize
+    # We refer to the initialize method as a constructor, because it gets triggered whenever we create a new object.
+    puts "This object was initialized!"
+  end
+end
+
+sparky = GoodDog.new
