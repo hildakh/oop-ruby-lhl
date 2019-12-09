84ab986f65f72b3f253b8eac9f7d6c586d0e3fa2
hildakh <h_khajehamiri@yahoo.com>
2019-12-09 23:55:26 +0000

    using constants in ruby

---
+++ constants.rb
@@ -0,0 +1,14 @@
+class GoodDog
+  DOG_YEARS = 7
+
+  attr_accessor :name, :age
+
+  def initialize(n, a)
+    self.name = n
+    self.age = a * DOG_YEARS
+  end
+end
+
+sparky = GoodDog.new("Sparky", 2)
+puts sparky.age
+# => 14
