2643ca860a39e840efd757b0d9cd5c4b04c2d03c
hildakh <h_khajehamiri@yahoo.com>
2019-12-10 03:40:42 +0000

    using ancestors

---
+++ method-lookup.rb
@@ -0,0 +1,34 @@
+module Walkable
+  def walk
+    "I'm walking."
+  end
+end
+
+module Swimmable
+  def swim
+    "I'm swimming."
+  end
+end
+
+module Climbable 
+  def climb
+    "I'm climbing."
+  end
+end
+
+class Animal
+  include Walkable
+
+  def speak
+    "I'm an animal, and I speak!"
+  end
+end
+
+puts "---Animal method lookup---"
+puts Animal.ancestors
+# => ---Animal method lookup---
+# Animal
+# Walkable
+# Object
+# Kernel
+# BasicObject
