c8e41527f4eac9eea15345e4d1e506b218512a44
hildakh <h_khajehamiri@yahoo.com>
2019-12-10 00:27:07 +0000

    added a comment on the to_s method

--- constants.rb
+++ constants.rb
@@ -12,5 +12,6 @@ end
 sparky = GoodDog.new("Sparky", 2)
 puts sparky
 # => #<GoodDog:0x007fe542323320>
+# The reason we get this particular output lies within the to_s method in Ruby. By default, the to_s method returns the name of the object's class and an encoding of the object id.
 puts sparky.age
 # => 14
